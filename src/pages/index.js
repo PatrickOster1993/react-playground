51738b757ea257535643a3df0d09e94c3b3fbc10
Patrick Oster <p.oster9330@googlemail.com>
2025-01-08 10:06:05 +0100

    cleaned project folders

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,6 @@
 // Importiert React und die Basisklasse Component, die für Klassenkomponenten verwendet wird.
 import React, { Component } from "react"
-// Importiert die navigate-Funktion von Gatsby, die für die Navigation zwischen Seiten verwendet wird.
-import { navigate } from "gatsby"
-// Importiert eine benutzerdefinierte Komponente, die in einem separaten Ordner definiert ist.
-import CustomButton from "../components/customButton"
+
 // Importiert globales Styling, um die gesamte Anwendung zu stylen.
 import GlobalStyle from "../styles/global"
 
@@ -15,15 +12,6 @@ export default class Index extends Component {
       <div>
         {/* GlobalStyle stellt globales Styling bereit */}
         <GlobalStyle />
-
-        {/* CustomButton ist eine benutzerdefinierte Komponente, die separat definiert wurde */}
-        <CustomButton />
-
-        {/* Button mit einem onClick-Handler, der die navigate-Funktion von Gatsby verwendet, 
-            um zur Impressum-Seite zu navigieren */}
-        <button onClick={() => navigate("/impressum")}>
-          Impressum besuchen!
-        </button>
       </div>
     )
   }
