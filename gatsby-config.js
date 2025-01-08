5a32d7628a47f4e30bf0e0ea48f6c650eee7dbea
Patrick Oster <p.oster9330@googlemail.com>
2025-01-08 04:43:01 +0100

    finished

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Basics`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components"]
+};
