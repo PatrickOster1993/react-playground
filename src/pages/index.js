5636ed5aecb6497641399aafa9db5aa0fbe79900
Philipp Heidenreich <Philipp_Heidenreich@gmx.de>
2025-01-08 11:25:00 +0100

    update compare to Main

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,8 @@
 // Importiert React und die Basisklasse Component, die für Klassenkomponenten verwendet wird.
 import React, { Component } from "react"
-// Importiert die navigate-Funktion von Gatsby, die für die Navigation zwischen Seiten verwendet wird.
-import { navigate } from "gatsby"
-// Importiert eine benutzerdefinierte Komponente, die in einem separaten Ordner definiert ist.
-import CustomButton from "../components/customButton"
+
 // Importiert globales Styling, um die gesamte Anwendung zu stylen.
 import GlobalStyle from "../styles/global"
-
-import wuschel from "../images/low-fi.jfif"
 
 // Die Index-Klasse ist die Hauptseite der Anwendung. Sie wird beim Start geladen.
 export default class Index extends Component {
@@ -17,15 +12,6 @@ export default class Index extends Component {
       <div>
         {/* GlobalStyle stellt globales Styling bereit */}
         <GlobalStyle />
-
-        {/* CustomButton ist eine benutzerdefinierte Komponente, die separat definiert wurde */}
-        <CustomButton />
-
-        {/* Button mit einem onClick-Handler, der die navigate-Funktion von Gatsby verwendet, 
-            um zur Impressum-Seite zu navigieren */}
-        <button onClick={() => navigate("/impressum")}>
-          Impressum besuchen!
-        </button>
       </div>
     )
   }
