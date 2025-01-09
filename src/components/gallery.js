cf6c07c9681c30c6e715b8354c7df94ab56e0051
Patrick Oster <p.oster9330@googlemail.com>
2025-01-09 12:31:22 +0100

    added search input bar

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -6,14 +6,16 @@ class Gallery extends Component {
     super(props)
     this.state = {
       pictures: this.props.pics.map((pic) => ({ ...pic, like: false })), // Jedes Bild erhält seinen eigenen "like"-Zustand
+      searchQuery: "",
     }
   }
 
   deletePicture = (indexToRemove) => {
-    const confirmDelete = window.confirm(
-      "Möchten Sie das Bild wirklich löschen oder sind sie spontan wahnsinnig geworden (und wissen es aber vielleicht nicht)?"
+    // Bestätigungsabfrage vor dem Löschen
+    const userConfirmed = window.confirm(
+      "Möchten Sie das Bild wirklich löschen?"
     )
-    if (confirmDelete) {
+    if (userConfirmed) {
       const pictures = [...this.state.pictures] // Kopie des Arrays erstellen
       pictures.splice(indexToRemove, 1) // Bild entfernen
       this.setState({ pictures }) // State aktualisieren
@@ -26,19 +28,40 @@ class Gallery extends Component {
     this.setState({ pictures }) // State aktualisieren
   }
 
+  handleSearch = (event) => {
+    this.setState({ searchQuery: event.target.value })
+  }
+
   render() {
-    return this.state.pictures.map((image, index) => (
-      <div key={index} className="gallery-element">
-        <button onClick={() => this.deletePicture(index)}>🗑️</button>
-        <Picture
-          src={image.src}
-          alt={image.alt}
-          desc={image.desc}
-          like={image.like} // Like-Status wird jetzt über props übergeben
-          toggleLike={() => this.toggleLike(index)} // Funktion, um den Like-Status zu ändern
+    const filteredPictures = this.state.pictures.filter((picture) =>
+      picture.desc.toLowerCase().includes(this.state.searchQuery.toLowerCase())
+    )
+
+    return (
+      <div>
+        <input
+          type="text"
+          placeholder="Suchen..."
+          value={this.state.searchQuery}
+          onChange={this.handleSearch}
         />
+
+        <div className="gallery">
+          {filteredPictures.map((image, index) => (
+            <div key={index} className="gallery-element">
+              <button onClick={() => this.deletePicture(index)}>🗑️</button>
+              <Picture
+                src={image.src}
+                alt={image.alt}
+                desc={image.desc}
+                like={image.like} // Like-Status wird jetzt über props übergeben
+                toggleLike={() => this.toggleLike(index)} // Funktion, um den Like-Status zu ändern
+              />
+            </div>
+          ))}
+        </div>
       </div>
-    ))
+    )
   }
 }
 
