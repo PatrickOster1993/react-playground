c75d623a961918c84577580a21d5ea2d1329c23d
Dirk Schef <1932163496@bildung.daa.de>
2025-01-09 09:47:00 +0100

    codereparatur

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,31 +2,31 @@ import React, { Component } from "react"
 import GlobalStyle from "../styles/global"
 
 // Importiere die Bilder direkt
-import wuschl from "../images/keine_eule.jpg"
-import musiker from "../images/musik_und_so.jpg"
-import dahoam from "../images/dahoam_is_dahoam.png"
-import blondie from "../images/blond_und_so.jpg"
-import löwe from "../images/einmal_loewe_immer_loewe.jpg"
-import skater from "../images/hals_und_beinbruch.jpg"
-import boy from "../images/lang_ists_her.png"
-import mode from "../images/modegeschmack_pur.jpg"
-import sis from "../images/schwester_und_ich.jpg"
-import au from "../images/traumhaft_schoen.png"
+import colanase from "../images/colanase.jpg"
+import kinder from "../images/kinder.jpg"
+import bordercolour from "../images/bordercolour.jpg"
+import cgrid from "../images/cgrid.png"
+import computersagtnein from "../images/ComputerSagtNein.jpg"
+import kackklammern from "../images/kackklammern.jpg"
+import ninspiel from "../images/Ninspiel.png"
+import ohno from "../images/ohno.png"
+import rasPi from "../images/rasPi.png"
+import rehgips1 from "../images/rehgips1.jpg"
 
 // Eigene Components
 import Gallery from "../components/gallery"
 
 const pictures = [
-  { src: wuschl, alt: "Wuschl", desc: "Schöner Kater" },
-  { src: musiker, alt: "Musiker", desc: "Schöner Musiker" },
-  { src: dahoam, alt: "Schloss", desc: "Schönes Schloss" },
-  { src: blondie, alt: "Blond", desc: "Schöne Frisur" },
-  { src: löwe, alt: "1860", desc: "Schöner Verein" },
-  { src: skater, alt: "Sk8er", desc: "Schöner Trick" },
-  { src: boy, alt: "Boy", desc: "Schöner Boy" },
-  { src: mode, alt: "Mode", desc: "Schöne Hose" },
-  { src: sis, alt: "Sis", desc: "Schönes Bild" },
-  { src: au, alt: "Österreich", desc: "Schöner Ort" },
+  { src: colanase, alt: "Colanase" },
+  { src: kinder, alt: "Kinder" },
+  { src: bordercolour, alt: "Bordercolour" },
+  { src: cgrid, alt: "Cgrid" },
+  { src: computersagtnein, alt: "Computersagtnein" },
+  { src: kackklammern, alt: "kackklammern" },
+  { src: ninspiel, alt: "ninspiel" },
+  { src: ohno, alt: "ohno" },
+  { src: rasPi, alt: "rasPi" },
+  { src: rehgips1, alt: "rehgips1" }
 ]
 
 export default class Index extends Component {
